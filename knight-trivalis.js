7211ef51334d1eb80ccb523c9071c47605ebefe3
GHOST-Aram <owinoojala@gmail.com>
2023-04-06 19:31:06 +0300

    feat: Create 8 * 8 array to represent Chess baord

--- knight-trivalis.js
+++ knight-trivalis.js
@@ -7,8 +7,14 @@ const boardSquares = board.board
 
 //Create square nodes rows and columns
 for(let rank = 0; rank < 8; rank ++){
+    const row = []
     for(let file = 0; file < 8; file ++){
+        //Create new Node for every square
         const node = new Node(rank, file)
+
+        //Push new nodes into row
+        row.push(node)
     }
-    
+    //Append to board squares
+    boardSquares.push(row)
 }
