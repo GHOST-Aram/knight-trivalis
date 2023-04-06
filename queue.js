d6e21c26ee14e92377a0d7b456d38a934c7bb7b1
GHOST-Aram <owinoojala@gmail.com>
2023-04-06 22:15:59 +0300

    fix: Fix error in Queue Constructor, set length to collection length:

--- queue.js
+++ queue.js
@@ -8,7 +8,7 @@ export class Queue {
 
     constructor(){
         this.collection = []
-        this.length = this.collection.length
+        this.collection.length = this.collection.length
     }
 
     //Prepend an item at the end of the collection
